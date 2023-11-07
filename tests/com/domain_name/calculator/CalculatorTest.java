5b54a34b9de0b881ee2fd3c59622a5cdc0c3d14a
noaa <noaamaman325158@gmail.com>
2023-11-07 03:41:09 +0200

    Finish implementing two tests based on single method

--- tests/com/domain_name/calculator/CalculatorTest.java
+++ tests/com/domain_name/calculator/CalculatorTest.java
@@ -1,13 +1,30 @@
 package com.domain_name.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    @Test
     public void canAddZeroPlusZero(){
-        Calculator calc = new Calculator();
 
         int sum = calc.add(0, 0);
-        assertEquals(0, sum);
+        assertEquals(0, sum, "Was expecting sum of 0");
     }
+    @Test
+    public void canAddOnePlusOne(){
+
+        int sum = calc.add(1, 1);
+        assertEquals(2, sum);
+    }
+
 }
